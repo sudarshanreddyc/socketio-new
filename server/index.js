2cd15bbe368c15101a4af43bd89051309ffbdf91
sudarshanreddyc <sudarshanreddy.c123@gmail.com>
2022-12-29 07:11:24 +0100

    Added hosted url in origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://blog-frontend-b4d.pages.dev",
     methods: ["GET", "POST"],
   },
 });
